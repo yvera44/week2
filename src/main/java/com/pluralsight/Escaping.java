0fc14c5dd38bcb7b8a4f7e92de6d5315c2ac8005
YairVera <yairvera44@gmail.com>
2025-09-30 10:47:20 -0500

    run comparing strings

--- src/main/java/com/pluralsight/Escaping.java
+++ src/main/java/com/pluralsight/Escaping.java
@@ -11,8 +11,18 @@ public class Escaping {
 
         String message2 = """
                 This is a message.
-                It goes on and on blah, blah, blah.""";
+                It goes on and on blah, blah, blah.
+                """;
 
         System.out.println(message2);
+        runComparingStringsExample();
+    }
+    public static void runComparingStringsExample(){
+        String s1 = "ABC";
+        String s2 = "AbC";
+
+        if (s1.equalsIgnoreCase(s2)) {
+            System.out.println("They are equal");
+        }
     }
 }
