2ff8c4b43a42643cd9dfc4df9148ecaa7515f5dc
YairVera <yairvera44@gmail.com>
2025-10-01 14:52:45 -0500

    Car Example car2

--- src/main/java/com/pluralsight/ClassExample.java
+++ src/main/java/com/pluralsight/ClassExample.java
@@ -8,5 +8,11 @@ public class ClassExample {
         car1.setModel("Altima");
 
         System.out.println(car1.getMake() + " " + car1.getModel());
+
+        Car car2 = new Car();
+        car2.setMake("Honda");
+        car2.setModel("Accord");
+
+        System.out.println(car2.getMake() + " " + car2.getModel());
     }
 }
