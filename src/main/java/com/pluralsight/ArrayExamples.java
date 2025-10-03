7497989cd97f3ee13191d2442189025e43e6da4c
YairVera <yairvera44@gmail.com>
2025-10-03 12:52:09 -0500

    Arrays Examples

---
+++ src/main/java/com/pluralsight/ArrayExamples.java
@@ -0,0 +1,24 @@
+package com.pluralsight;
+
+public class ArrayExamples {
+    public static void main(String[] args) {
+        int[] numbers = {54, 23, 33, 94, 75, 67, 70, 89};
+
+        int numberA = numbers [3];
+        System.out.println(numberA);
+
+        int numberB = numbers [5];
+        System.out.println(numberB);
+
+        System.out.println(numbers[2]);
+
+        String[] bands = {"Megadeth", "Agalloch", "Trivium", "Deafheaven", "Signs of the Swarm"};
+
+        String band1 = bands[3];
+        System.out.println(band1);
+
+        System.out.println(bands[0]);
+
+
+    }
+}
